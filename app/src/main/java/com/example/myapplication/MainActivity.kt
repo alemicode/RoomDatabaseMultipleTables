6f7e6fb3974dc3ad295abd8000bd86ee3f8b0c61
Mohamad <mohamad.alemicode@gmail.com>
2023-12-20 21:24:05 +0330

    Design Room DB with Multiple Tables

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.plcoding.multipleroomtables.SchoolDatabase
+import com.plcoding.multipleroomtables.entities.Director
+import com.plcoding.multipleroomtables.entities.School
+import com.plcoding.multipleroomtables.entities.Student
+import com.plcoding.multipleroomtables.entities.Subject
+import com.plcoding.multipleroomtables.entities.relations.SchoolAndDirector
+import com.plcoding.multipleroomtables.entities.relations.SchoolWithStudents
+import com.plcoding.multipleroomtables.entities.relations.StudentSubjectCrossRef
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
